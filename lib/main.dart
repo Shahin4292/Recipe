aa1bf4f0e6f7342922a34b7691aa8c8fe25054a3
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-18 16:52:54 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/screens/on_boarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: OnBoardingScreen(),
     );
   }
 }
-
--- lib/screens/on_boarding_screen.dart
+++ lib/screens/on_boarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/utils/image_path.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -17,11 +18,38 @@ class OnBoardingScreen extends StatelessWidget {
                 child: Container(
                   height: size.height * 0.79,
                   width: size.width,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    image: DecorationImage(image: AssetImage("")),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                          ImagePath.onBoarding,
+                        ),
+                        fit: BoxFit.cover),
                   ),
-                ))
+                )),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: size.height * 0.243,
+                width: size.width,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(40),
+                        topLeft: Radius.circular(40))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Let's Cook Good Food",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: size.width * 0.06),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
