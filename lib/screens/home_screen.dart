9f40504202ec40b65972539f199a6418d0055fff
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-26 11:42:19 +0600

    explore image add done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../components/apps_bar.dart';
+import '../utils/image_path.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+          child: Column(
+            children: [
+              AppsBar(),
+              SizedBox(
+                height: size.height * 0.022,
+              ),
+              Container(
+                height: size.height * 0.22,
+                width: size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage(ImagePath.explore),fit: BoxFit.cover
+                ),),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
