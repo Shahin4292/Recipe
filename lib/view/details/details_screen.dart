afac28fabbcf6f6c54d46179377fc546277412d3
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-25 13:55:04 +0600

    show text add done

--- lib/view/details/details_screen.dart
+++ lib/view/details/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/utils/modify_text.dart';
 
 class DetailsScreen extends StatelessWidget {
   final String? imageUrl;
@@ -10,31 +11,41 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: [
-            Container(
-              height: 400,
-              width: MediaQuery.sizeOf(context).width,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  image: DecorationImage(
-                      image: NetworkImage(imageUrl!), fit: BoxFit.cover)),
+            Stack(
+              children: [
+                Container(
+                  height: 400,
+                  width: MediaQuery.sizeOf(context).width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: DecorationImage(
+                          image: NetworkImage(imageUrl!), fit: BoxFit.cover)),
+                ),
+                Positioned(
+                    top: 15,
+                    left: 15,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: Icon(
+                            Icons.arrow_back_ios_new,
+                            color: Colors.white,
+                            size: 20,
+                          )),
+                    ))
+              ],
             ),
-            Positioned(
-                top: 15,
-                left: 15,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      child: Icon(
-                        Icons.arrow_back_ios_new,
-                        color: Colors.white,
-                        size: 20,
-                      )),
-                ))
+            ModifyText(
+              text: name!,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
           ],
         ),
       ),
