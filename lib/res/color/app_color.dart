fea1a6a60d6ea4a3168965e72660a997e4ea322e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-25 11:40:30 +0600

    add done

---
+++ lib/res/color/app_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const Color blackColor = Color(0xff000000);
+
+  static const Color whiteColor = Color(0xffffffff);
+
+  static const Color primaryColor = Color(0xff01B1C9);
+
+  static const Color primaryButtonColor = Color(0xff01B1C9);
+  static const Color secondaryButtonColor = Color(0xffFC3F5B);
+
+  static const Color redColor = Color(0xffFC3F5B);
+
+  static const Color primaryTextColor = Color(0xff000000);
+  static const Color secondaryTextColor = Color(0xff444648);
+}
+
+class GradientColor {
+  static const LinearGradient gradientColor = LinearGradient(
+    colors: [Color(0xFFA0DAFB), Color(0xFF0A8ED9)],
+  );
+}
