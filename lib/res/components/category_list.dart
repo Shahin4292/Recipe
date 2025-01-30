1662c908ec6471184eb305748e58c11ec58f25c9
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-30 19:00:20 +0600

    add list of category image

---
+++ lib/res/components/category_list.dart
@@ -0,0 +1,20 @@
+import 'package:recipe/res/assets/image_path.dart';
+
+List<String> name = [
+  "BreakFast",
+  "Lunch",
+  "Dinner",
+  "Supper"
+];
+
+List<String> image = [
+  ImagePath.drink,
+  ImagePath.fastFood,
+  ImagePath.fishAndChips,
+  ImagePath.iceCream,
+  ImagePath.orangeJuice,
+  ImagePath.popcorn,
+  ImagePath.sweets,
+  ImagePath.vegetable,
+  ImagePath.tasty,
+];
