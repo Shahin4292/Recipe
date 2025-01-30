d94759551c7e0dd2604338ec48cf85203425be76
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-30 20:35:34 +0600

    add all image path

--- lib/res/assets/image_path.dart
+++ lib/res/assets/image_path.dart
@@ -1,4 +1,18 @@
 class ImagePath {
-  static String onBoarding = "assets/images/onboarding_profile.png";
-  static String explore = "assets/images/explore.png";
+  static String breakFast = "assets/images/break_fast.jpg";
+  static String lunch = "assets/images/lunch.jpg";
+  static String dinner = "assets/images/dinner.jpg";
+  static String supper = "assets/images/supper.jpg";
+
+  static String onBoarding = "assets/png/onboarding_profile.png";
+  static String explore = "assets/png/explore.png";
+  static String drink = "assets/png/drink.png";
+  static String fastFood = "assets/fast_food.png";
+  static String fishAndChips = "assets/fish_and_chips.png";
+  static String iceCream = "assets/ice_cream.png";
+  static String orangeJuice = "assets/orange_juice.png";
+  static String popcorn = "assets/popcorn.png";
+  static String sweets = "assets/sweets.png";
+  static String tasty = "assets/tasty.png";
+  static String vegetable = "assets/vegetable.png";
 }
