6bf8e56dd363c0c129dde26643367b944baa329f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-08 11:17:11 +0600

    all done

--- lib/res/components/category_list.dart
+++ lib/res/components/category_list.dart
@@ -1,20 +1,34 @@
 import 'package:recipe/res/assets/image_path.dart';
 
-List<String> name = [
-  "BreakFast",
-  "Lunch",
-  "Dinner",
-  "Supper"
-];
+List<String> name = ["BreakFast", "Lunch", "Dinner", "Supper"];
 
 List<String> image = [
+  ImagePath.breakFast,
+  ImagePath.lunch,
+  ImagePath.dinner,
+  ImagePath.supper,
+];
+
+List<String> categories = [
+  "Chocolate",
+  "Drink",
+  "Fast Food",
+  "Fish",
+  "Ice Cream",
+  "Juice",
+  "Pop Corn",
+  "Vegetable",
+  "Tasty",
+];
+
+List<String> categoryImage = [
+  ImagePath.sweets,
   ImagePath.drink,
   ImagePath.fastFood,
   ImagePath.fishAndChips,
   ImagePath.iceCream,
   ImagePath.orangeJuice,
   ImagePath.popcorn,
-  ImagePath.sweets,
   ImagePath.vegetable,
   ImagePath.tasty,
 ];
