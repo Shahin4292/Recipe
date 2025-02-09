5634b2398ca1bf38ba9ddc0914feae91d4185b65
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-09 11:35:58 +0600

    show grid image done

--- lib/view/all_recipe/all_recipe.dart
+++ lib/view/all_recipe/all_recipe.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../data/response/status.dart';
+import '../../res/components/general_exception.dart';
+import '../../res/components/internet_exceptions_widget.dart';
+import '../../viewModel/controller/home controller.dart';
 
 class AllRecipe extends StatelessWidget {
-  const AllRecipe({super.key});
+  final String recipe;
+  AllRecipe({super.key, required this.recipe});
+
+  HomeController homeController = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: Obx(() {
+        switch (homeController.rxRequestStatus.value) {
+          case Status.LOADING:
+            return const Center(child: CircularProgressIndicator());
+          case Status.ERROR:
+            if (homeController.error.value == 'No internet') {
+              return InterNetExceptionWidget(
+                onPress: () {
+                  homeController.refreshApi();
+                },
+              );
+            } else {
+              return GeneralExceptionWidget(onPress: () {
+                homeController.refreshApi();
+              });
+            }
+          case Status.COMPLETE:
+            return GridView.builder(
+                itemCount: homeController.userList.value.recipes!.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: .6 / 10),
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        Expanded(
+                            flex: 3,
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(20),
+                                      image: DecorationImage(
+                                          image: NetworkImage(homeController
+                                              .userList
+                                              .value
+                                              .recipes![index]
+                                              .image
+                                              .toString()),
+                                          fit: BoxFit.cover)),
+                                )
+                              ],
+                            )),
+                        Expanded(flex: 1, child: SizedBox())
+                      ],
+                    ),
+                  );
+                });
+        }
+      }),
     );
   }
 }
